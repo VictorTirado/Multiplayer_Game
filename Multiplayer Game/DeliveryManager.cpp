677b7002bc6e575ac5120437216b5eb7d9dc2526
MAtaur00 <mtmtarres@gmail.com>
2020-12-24 16:49:39 +0100

    delivery manager

--- Multiplayer Game/DeliveryManager.cpp
+++ Multiplayer Game/DeliveryManager.cpp
@@ -1,3 +1,117 @@
 #include "Networks.h"
 
-// TODO(you): Reliability on top of UDP lab session
+Delivery* DeliveryManager::WriteSequenceNumber(OutputMemoryStream& packet)
+{
+	packet << nextSeqNum;
+
+	Delivery* delivery = new Delivery;
+	delivery->delegate = new DeliveryDelegate;
+
+	delivery->dispatchTime = Time.time;
+	delivery->sequenceNumber = nextSeqNum;
+	nextSeqNum++;
+
+	pendingDeliveries.push_back(delivery);
+
+	return delivery;
+}
+
+bool DeliveryManager::ProcessSequenceNumber(const InputMemoryStream& packet)
+{
+	bool ret = false;
+
+	uint32 currSeqNum; packet >> currSeqNum;
+
+	if (currSeqNum >= nextSeqNumExpected)
+	{
+		pendingNumSeqList.push_back(currSeqNum);
+		nextSeqNumExpected = currSeqNum + 1;
+		ret = true;
+	}
+
+	return ret;
+}
+
+bool DeliveryManager::HasSequenceNumbersPendingAck() const
+{
+	return !pendingNumSeqList.empty();
+}
+
+void DeliveryManager::WriteSequenceNumbersPendingAck(OutputMemoryStream& packet)
+{
+	packet << pendingNumSeqList.size();
+	for (std::list<uint32>::const_iterator it = pendingNumSeqList.begin(); it != pendingNumSeqList.end(); ++it)
+	{
+		packet << *it;
+	}
+	pendingNumSeqList.clear();
+}
+
+void DeliveryManager::ProcessAckdSequenceNumbers(const InputMemoryStream& packet)
+{
+	int size; packet >> size;
+
+	for (int it = 0; it < size; ++it)
+	{
+		uint32 seqNum; packet >> seqNum;
+
+		for (auto delivery : pendingDeliveries)
+		{
+			if (delivery->sequenceNumber == seqNum)
+			{
+				delivery->delegate->OnDeliverySuccess(this);
+				pendingDeliveries.remove(delivery);
+				delete delivery->delegate;
+				delete delivery;
+				break;
+			}
+		}
+	}
+}
+
+void DeliveryManager::ProcessTimedOutPackets()
+{
+	std::list<std::list<Delivery*>::iterator> toDelete;
+
+	for (std::list<Delivery*>::iterator it = pendingDeliveries.begin(); it != pendingDeliveries.end(); ++it)
+	{
+		if (Time.time - (*it)->dispatchTime >= PACKET_DELIVERY_TIMEOUT_SECONDS)
+		{
+			(*it)->delegate->OnDeliveryFailure(this);
+			toDelete.push_back(it);
+			delete (*it)->delegate;
+			delete* it;
+		}
+	}
+
+	for (auto mode : toDelete)
+	{
+		pendingDeliveries.erase(mode);
+	}
+}
+
+void DeliveryManager::Clear()
+{
+	for (auto delivery : pendingDeliveries)
+	{
+		delete delivery->delegate;
+		delete delivery;
+	}
+	pendingDeliveries.clear();
+	pendingNumSeqList.clear();
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------DELIVERY DELEGATE-----------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
+
+void DeliveryDelegate::OnDeliverySuccess(DeliveryManager* deliveryManager)
+{
+	//We have no idea
+}
+
+void DeliveryDelegate::OnDeliveryFailure(DeliveryManager* deliveryManager)
+{
+	//virtual?
+}
--- Multiplayer Game/DeliveryManager.h
+++ Multiplayer Game/DeliveryManager.h
@@ -1,3 +1,57 @@
 #pragma once
 
-// TODO(you): Reliability on top of UDP lab session
+#include <list>
+class DeliveryManager;
+
+class DeliveryDelegate
+{
+public:
+	void OnDeliverySuccess(DeliveryManager* deliveryManager);
+	void OnDeliveryFailure(DeliveryManager* deliveryManager);
+
+};
+struct Delivery
+{
+	uint32 sequenceNumber = 0;
+	double dispatchTime = 0.0;
+	DeliveryDelegate* delegate = nullptr;
+};
+
+
+
+class DeliveryManager
+{
+public:
+	// For senders to write a new seq. numbers into a packet 
+	Delivery* WriteSequenceNumber(OutputMemoryStream& packet);
+
+	// For receivers to process the seq. number from an incoming packet 
+	bool ProcessSequenceNumber(const InputMemoryStream& packet);
+
+	// For receivers to write ack'ed seq. numbers into a packet 
+	bool HasSequenceNumbersPendingAck() const;
+	void WriteSequenceNumbersPendingAck(OutputMemoryStream& packet);
+
+	// For senders to process ack'ed seq. numbers from a packet
+	void ProcessAckdSequenceNumbers(const InputMemoryStream& packet);
+	void ProcessTimedOutPackets();
+
+	void Clear();
+
+private:
+
+
+	// Private members (receiver side)
+	// - The next expected sequence number
+	uint32 nextSeqNumExpected = 0u;
+
+	// - A list of sequence numbers pending ack
+	std::list<uint32> pendingNumSeqList;
+
+	// Private members (sender side)
+	// - The next outgoing sequence number
+	uint32 nextSeqNum = 0u;
+public:
+	// - A list of pending deliveries
+	std::list<Delivery*> pendingDeliveries;
+};
